e958af251b7744153716bf1ab9110f06ba8acf19
Kanda, Jiří <kanda@havit.cz>
2021-10-13 07:36:01 +0200

    Initial commit

---
+++ ClassLibrary1/HxGrid.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    [CascadingTypeParameter(nameof(TItem))]
+    public class HxGrid<TItem> : ComponentBase
+    {
+        [Parameter] public RenderFragment Columns { get; set; }
+    }
+}
